7a9ce2ef29f488e932ddddd43967ad7c61eb433e
Carl Redding <credding07@gmail.com>
2020-05-28 16:50:34 -0500

    declaring variables and outputting them with the print function.

--- variables.py
+++ variables.py
@@ -14,3 +14,20 @@ VARIABLE RULES:
 - Can have numbers but can not start with one
 """
 
+# Main types you will be dealing with
+
+# # int
+# x = 1
+# # float
+# y = 2.5
+# # string
+# name = 'John'
+# # bool
+# is_cool = True
+
+# Multiple assignment
+x, y, name, is_cool, = (1, 2.5, 'John', True)
+
+# print() is an output function in python like console.log in javascript
+# print('Hola!')
+print(x, y, name, is_cool)
