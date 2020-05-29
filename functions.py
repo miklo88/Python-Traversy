de6b24aea88bb0fead20801338424a46c50a01e7
Carl Redding <credding07@gmail.com>
2020-05-29 12:59:03 -0500

    turned format on save off because you cannot write lambda functions with it turned on. look more into a way of working around that. or possibly just not use lambda functions.

--- functions.py
+++ functions.py
@@ -2,11 +2,33 @@
 
 # Create function
 def sayHello(name):
-    print(f'Hello {name}')
+    print(f'Hola {name}')
 
 
 sayHello('Carlitos')
 
-# A lambda function is a small anonymous function.
 
+# can also invoke function with no argument
+def sayHello(name='Muchachicho'):
+    print(f'Hola {name}')
+
+
+sayHello()
+
+
+# Return values
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+# most of the time you'll be assigning it to a variable
+num = getSum(3, 14)
+print(num)
+
+# A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+#   implicitly returns
+getSum = lambda num1, num2 : num1 + num2
+
+print(getSum(10, 17))
